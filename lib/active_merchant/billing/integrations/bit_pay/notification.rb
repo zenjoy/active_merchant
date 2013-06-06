5547910c746daa69d9809e5c9b9c38e8cbb4f225
Justin Li <jli.justinli@gmail.com>
2013-06-06 13:59:46 -0400

    Implement aknowledge

--- lib/active_merchant/billing/integrations/bit_pay/notification.rb
+++ lib/active_merchant/billing/integrations/bit_pay/notification.rb
@@ -5,6 +5,11 @@ module ActiveMerchant #:nodoc:
     module Integrations #:nodoc:
       module BitPay
         class Notification < ActiveMerchant::Billing::Integrations::Notification
+          def initialize(post, options = {})
+            super
+
+          end
+
           def complete?
             ['complete', 'confirmed'].include?(status)
           end
@@ -15,6 +20,10 @@ module ActiveMerchant #:nodoc:
 
           def transaction_id
             params['id']
+          end
+
+          # When was this payment received by the client.
+          def received_at
           end
 
           def payer_email
@@ -52,9 +61,12 @@ module ActiveMerchant #:nodoc:
           def acknowledge
             payload = raw
 
+            response = ssl_get(BitPay.service_url + "/invoice/#{transaction_id}")
+            data = JSON.parse(response.body)
+
             # Replace with the appropriate codes
-            raise StandardError.new("Faulty BitPay result: #{response.body}") unless ["AUTHORISED", "DECLINED"].include?(response.body)
-            response.body == "AUTHORISED"
+            raise StandardError.new("Faulty BitPay result: #{response.body}") unless data.has_key?(:status)
+            data.status == "confirmed" || data.status == "complete"
           end
 
           private
@@ -62,6 +74,16 @@ module ActiveMerchant #:nodoc:
           # Take the posted data and move the relevant data into a hash
           def parse(post)
             @params = JSON.parse(post)
+          end
+
+          def ssl_get(url, options={})
+            uri = URI.parse(url)
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+
+            request = Net::HTTP::Get.new(uri.request_uri)
+            request.basic_auth(options[:auth_user], options[:auth_pass])
+            http.request(request)
           end
         end
       end
